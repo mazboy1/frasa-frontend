bf2f59366559ec58a738daa6f96e91f23738cf8b
sealkea <sealkea2@gmail.com>
2026-04-02 11:17:10 +0700

    admin stats yok bisa lagi

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    minify: 'terser',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          firebase: ['firebase/app', 'firebase/auth'],
-          axios: ['axios'],
-        }
-      }
-    }
-  },
-  server: {
-    middlewareMode: false
+    sourcemap: false
   }
 })
